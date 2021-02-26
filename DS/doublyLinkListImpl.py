79596d959eb84716774a673f99ddf662bdbf6802
sandeeppagi <sandippagi@gmail.com>
2021-02-27 00:38:20 +0530

    Added add node before/after

--- DS/doublyLinkListImpl.py
+++ DS/doublyLinkListImpl.py
@@ -38,6 +38,41 @@ class DoublyLinkedList:
             self.head = node
             return
 
+    def add_after(self, key, data):
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                break
+            curr = curr.next
+        if curr is None:
+            print('Key not found')
+        else:
+            node = Node(data)
+            node.next = curr.next
+            node.prev = curr
+            if curr.next:
+                curr.next.prev = node
+            curr.next = node
+
+    def add_before(self, key, data):
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                break
+            curr = curr.next
+        if curr is None:
+            print('Key not found')
+        else:
+            node = Node(data)
+            node.next = curr
+            node.prev = curr.prev
+            if curr.prev:
+                curr.prev.next = node
+            curr.prev = node
+            if curr == self.head:
+                self.head = node
+
+
     def my_print(self):
         curr = self.head
         while curr:
@@ -58,3 +93,15 @@ mydll_1.prepend(88)
 mydll_1.prepend(2)
 mydll_1.prepend(3)
 mydll_1.my_print()
+print('-'*30)
+print('Add after the key')
+mydll_2 = DoublyLinkedList()
+mydll_2.append(1)
+mydll_2.add_after(1, 2)
+mydll_2.my_print()
+print('Add before the key')
+mydll_3 = DoublyLinkedList()
+mydll_3.append(1)
+mydll_3.append(2)
+mydll_3.add_before(2, 3)
+mydll_3.my_print()
