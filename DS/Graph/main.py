1920d2545ffde97bb9f7c3b62b02643d9a96e16e
sandeeppagi <sandippagi@gmail.com>
2021-03-10 14:16:16 +0530

    dsf traversal

--- DS/Graph/main.py
+++ DS/Graph/main.py
@@ -18,7 +18,6 @@ g.add_edge(1, 3)
 g.add_edge(1, 4)
 g.print_graph()
 
-
 # directed graph
 g1 = Graph(4, False)
 g1.add_edge(0, 1)
@@ -50,6 +49,7 @@ def bfs_traversal(g, source):
 bfs_traversal(g, 0)
 bfs_traversal(g1, 0)
 
+
 # TODO: Refactor below method
 def dsf_traversal_recursive(g, source, result, stack):
     if source < g.vertices:
@@ -68,37 +68,79 @@ def dsf_traversal_recursive(g, source, result, stack):
                     dsf_traversal_recursive(g, node.next.data, result, stack)
     return result
 
+
 def dsf_traversal_iterative(g, source):
     result = []
+    my_stack = Stack()
+    my_stack.push(source)
+    while my_stack.is_empty() is False:
+        curr_node = my_stack.pop()
+        result.append(curr_node)
+        node = g.array[curr_node].get_head()
+        while node:
+            if node.data not in result:
+                my_stack.push(node.data)
+            node = node.next
+    return "".join([str(i) for i in result])
+
+
+def dfs_traversal_helper(g, source, visited):
+    result = ""
+    # Create Stack(Implemented in previous lesson) for Depth First Traversal
+    # and Push source in it
     stack = Stack()
-    if source < g.vertices:
-        stack.push(source)
-        number_of_nodes = len(g.array)
-        while len(result) != number_of_nodes :
-            if source not in result:
-                result.append(source)
-            node = g.array[source].get_head()
-            if node:
-                if node.data not in result:
-                    stack.push(node.data)
-                    source = node.data
-                elif node.next:
-                    stack.push(node.next.data)
-                    source = node.next.data
-            if node is None:
-                source = stack.pop()
-        return "".join([str(i) for i in result])
+    stack.push(source)
+    visited[source] = True
+    # Traverse while stack is not empty
+    while (stack.is_empty() is False):
+        # Pop a vertex/node from stack and add it to the result
+        current_node = stack.pop()
+        result += str(current_node)
+        # Get adjacent vertices to the current_node from the array,
+        # and if they are not already visited then push them in the stack
+        temp = g.array[current_node].get_head()
+        while (temp is not None):
+            if (visited[temp.data] is False):
+                stack.push(temp.data)
+                # Visit the node
+                visited[temp.data] = True
+            temp = temp.next
+    return result, visited  # For the above graph it should return "12453"
 
 
+def dfs_traversal(g, source):
+    result = ""
+    num_of_vertices = g.vertices
+    if num_of_vertices == 0:
+        return result
+    # A list to hold the history of visited nodes
+    # Make a node visited whenever you enqueue it into queue
+    visited = []
+    for i in range(num_of_vertices):
+        visited.append(False)
+    # Start from source
+    result, visited = dfs_traversal_helper(g, source, visited)
+    # visit remaining nodes
+    for i in range(num_of_vertices):
+        if visited[i] is False:
+            result_new, visited = dfs_traversal_helper(g, i, visited)
+            result += result_new
+    return result
 
-g2 = Graph(6, False)
+
+g2 = Graph(9, False)
+g2.add_edge(0, 6)
 g2.add_edge(0, 2)
 g2.add_edge(0, 1)
 g2.add_edge(1, 4)
 g2.add_edge(1, 3)
 g2.add_edge(2, 5)
+g2.add_edge(6, 8)
+g2.add_edge(6, 7)
 g2.print_graph()
 
 stack = Stack()
 print(dsf_traversal_recursive(g2, 0, [], stack))
 print(dsf_traversal_iterative(g2, 0))
+
+print(dfs_traversal(g2, 0))
