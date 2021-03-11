eec96ed4c5cb74988677dc808b76ea7a632c299d
sandeeppagi <sandippagi@gmail.com>
2021-03-11 13:24:13 +0530

    Added recursive method for cyclic graph detection

--- DS/Graph/cyclic_graph.py
+++ DS/Graph/cyclic_graph.py
@@ -24,6 +24,51 @@ def is_cyclic_graph(g, source):
     return False
 
 
+def detect_cycle(g):
+    g.print_graph()
+    # visited list to keep track of the nodes that have been visited
+    # since the beginning of the algorithm
+    visited = [False] * g.vertices
+
+    # rec_node_stack keeps track of the nodes which are part of the
+    # current recursive call
+    rec_node_stack = [False] * g.vertices
+
+    for node in range(g.vertices):
+        # DFS recursion call
+        if detect_cycle_rec(g, node, visited, rec_node_stack):
+            return True
+
+    return False
+
+
+def detect_cycle_rec(g, node, visited, rec_node_stack):
+    # Node was already in recursion stack. Cycle found.
+    if rec_node_stack[node]:
+        return True
+
+    # It has been visited before this recursion
+    if visited[node]:
+        return False
+    # Mark current node as visited and
+    # add to recursion stack
+    visited[node] = True
+    rec_node_stack[node] = True
+
+    head_node = g.array[node].get_head()
+    while head_node is not None:
+        # Pick adjacent node and call it recursively
+        adjacent = head_node.data
+        # If the node is visited again in the same recursion => Cycle found
+        if detect_cycle_rec(g, adjacent, visited, rec_node_stack):
+            return True
+        head_node = head_node.next
+    # remove the node from the recursive call
+    rec_node_stack[node] = False
+    return False
+
+
+
 # directed graph
 g = Graph(5, False)
 g.add_edge(0, 1)
@@ -54,9 +99,16 @@ g3.add_edge(0, 1)
 g3.add_edge(1, 2)
 g3.add_edge(2, 0)
 
-print('-' * 45)
+print('-' * 50)
 print('Check Cyclic graph iterative')
 print(is_cyclic_graph(g, 0))
 print(is_cyclic_graph(g1, 0))
 print(is_cyclic_graph(g2, 0))
 print(is_cyclic_graph(g3, 0))
+
+print('-' * 50)
+print('Check Cyclic graph recursive V2')
+print(detect_cycle(g))
+print(detect_cycle(g1))
+print(detect_cycle(g2))
+print(detect_cycle(g3))
