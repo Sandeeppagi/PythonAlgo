2f47c47dbd1ac2d18b3c32e86f347cefda1a5f57
sandeeppagi <sandippagi@gmail.com>
2021-03-13 08:15:07 +0530

    Added UI for Graph print

--- DS/Graph/graph.py
+++ DS/Graph/graph.py
@@ -1,23 +1,44 @@
 from LinkedList.linked_list import LinkedList
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 class Graph:
     def __init__(self, vertices, is_undirected):
+        self.nx = nx
+        self.plt = plt
+        self.directed = nx.DiGraph()
+        self.undirected = nx.Graph()
         self.vertices = vertices
         self.array = []
         self.is_undirected = is_undirected
         for i in range(vertices):
             vertex = LinkedList()
             self.array.append(vertex)
+            if self.is_undirected:
+                self.undirected.add_node(i)
+            else:
+                self.directed.add_node(i)
 
     def add_edge(self, source, destination):
         if source < self.vertices and destination < self.vertices:
+            if self.is_undirected:
+                self.undirected.add_edge(source, destination)
+            else:
+                self.directed.add_edge(source, destination)
+
             self.array[source].insert_at_head(destination)
             if self.is_undirected:
                 self.array[destination].insert_at_head(source)
 
+
     def print_graph(self):
-        print('-'*50)
+        if self.is_undirected:
+            self.nx.draw(self.undirected, with_labels=1)
+        else:
+            self.nx.draw(self.directed, with_labels=1)
+        self.plt.show()
+        print('-' * 50)
         print(f'Printing adjacency list of the {"undirected" if self.is_undirected else "directed"} graphs')
         print()
         for i in range(self.vertices):
