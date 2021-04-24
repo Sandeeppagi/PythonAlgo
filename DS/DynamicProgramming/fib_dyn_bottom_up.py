8e277b02a9845a966af4edf2b79bc5e1213cc209
sandeeppagi <sandippagi@gmail.com>
2021-04-24 23:28:17 +0530

    Fibonacci bottom up approach

--- DS/DynamicProgramming/fib_dyn_bottom_up.py
+++ DS/DynamicProgramming/fib_dyn_bottom_up.py
@@ -9,5 +9,22 @@ def fib_bottom_up(number):
     return table[number]
 
 
-num = 1000
+def fib_bottom_up_space(n):
+    if n == 0:  # base cases
+        return 0
+    if n == 1:  # base cases
+        return 1
+    secondLast = 0  # base case 1, fib(0) = 0
+    last = 1  # base case 2, fib(1) = 1
+    current = None  # initially set to None
+    for i in range(1, n):  # iterate n times to evaluate n-th fibonacci
+        # storing ith fibonacci in current by summing up i-1th and i-2th fibonacci
+        current = secondLast + last
+        secondLast = last  # updating for next iteration
+        last = current
+    return current  # return the value of n in tabulation table
+
+
+num = 6
 print(f"Fibonacci for number {num} is {fib_bottom_up(num)}")
+print(f"Fibonacci for number {num} is {fib_bottom_up_space(num)}")
