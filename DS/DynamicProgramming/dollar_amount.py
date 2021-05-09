c748b01100baf3a2069503408041a88c1bf907cb
sandeeppagi <sandippagi@gmail.com>
2021-05-09 22:38:20 +0530

    ways of dollar amount

--- DS/DynamicProgramming/dollar_amount.py
+++ DS/DynamicProgramming/dollar_amount.py
@@ -11,9 +11,24 @@ def countways_(bills, amount, maximum):
     return ways
 
 
-
 def countways(bills, amount):
     return countways_(bills, amount, max(bills))
 
 
 print(countways([1, 2, 5], 5))
+
+
+def countways_v1_(bills, amount, index):
+    if amount == 0:  # base case 1
+        return 1
+    if amount < 0 or index >= len(bills):  # base case 2
+        return 0
+    # count the number of ways to make amount by including bills[index] and excluding bills[index]
+    return countways_v1_(bills, amount - bills[index], index) + countways_v1_(bills, amount, index + 1)
+
+
+def countways_v1(bills, amount):
+    return countways_v1_(bills, amount, 0)
+
+
+print(countways_v1([1, 2, 5], 5))
