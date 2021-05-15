59bb16de5da8bdb86c7121a81d9f86685d1303e1
sandeeppagi <sandippagi@gmail.com>
2021-05-15 21:06:03 +0530

    merge sort

--- DS/Lists/find_min.py
+++ DS/Lists/find_min.py
@@ -1,38 +1,38 @@
-def merge_sort(my_list):
-    if len(my_list) > 1:
-        mid = len(my_list) // 2
-        left = my_list[:mid]
-        right = my_list[mid:]
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left = lst[:mid]
+        right = lst[mid:]
 
         merge_sort(left)
         merge_sort(right)
 
-        i = 0
-        j = 0
-        k = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                my_list[k] = left[i]
-                i += 1
-            else:
-                my_list[k] = right[j]
-                j += 1
-            k += 1
-        while i < len(left):
-            my_list[k] = left[i]
-            i += 1
-            k += 1
-        while j < len(right):
-            my_list[k] = left[j]
-            j += 1
-            k += 1
+        index_lst1 = 0
+        index_lst2 = 0
+        index = 0
+        while index_lst1 < len(left) and index_lst2 < len(right):
+            if left[index_lst1] < right[index_lst2]:
+                lst[index] = left[index_lst1]
+                index_lst1 += 1
+            elif right[index_lst2] < left[index_lst1]:
+                lst[index] = right[index_lst2]
+                index_lst2 += 1
+            index += 1
+        while index_lst1 < len(left):
+            lst[index] = left[index_lst1]
+            index_lst1 += 1
+            index += 1
+        while index_lst2 < len(right):
+            lst[index] = right[index_lst2]
+            index_lst2 += 1
+            index += 1
 
 
 def find_minimum(lst):
     if len(lst) <= 0:
         return None
-    merge_sort(lst)  # sort list
-    return lst[0]  # return first element
+    merge_sort(lst)
+    return lst[0]
 
 
 print(find_minimum([9, 2, 3, 6]))
