0f3f58d13ddc510810c37a102377a794252be039
sandeeppagi <sandippagi@gmail.com>
2021-05-15 21:01:35 +0530

    merge sort

---
+++ DS/Lists/find_min.py
@@ -0,0 +1,38 @@
+def merge_sort(my_list):
+    if len(my_list) > 1:
+        mid = len(my_list) // 2
+        left = my_list[:mid]
+        right = my_list[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                my_list[k] = left[i]
+                i += 1
+            else:
+                my_list[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            my_list[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            my_list[k] = left[j]
+            j += 1
+            k += 1
+
+
+def find_minimum(lst):
+    if len(lst) <= 0:
+        return None
+    merge_sort(lst)  # sort list
+    return lst[0]  # return first element
+
+
+print(find_minimum([9, 2, 3, 6]))
