d19ab7295edc7568bb26b869ec69a38405fe187a
sandeeppagi <sandippagi@gmail.com>
2021-05-25 20:05:17 +0530

    Kadane Algo

---
+++ DS/Lists/kadane_algo.py
@@ -0,0 +1,19 @@
+def find_max_sum_sublist(lst):
+    if len(lst) < 1:
+        return 0
+    curr_max = lst[0];
+    global_max = lst[0]
+    len_of_lst = len(lst)
+
+    for i in range(1, len_of_lst):
+        if curr_max < 0:
+            curr_max = lst[i]
+        else:
+            curr_max += lst[i]
+        if global_max < curr_max:
+            global_max = curr_max
+    return global_max
+
+
+lst = [-4, 2, -5, 1, 2, 3, 6, -5, 1]
+print("Sum of largest subarray: ", find_max_sum_sublist(lst))
