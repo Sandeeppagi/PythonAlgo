a6f7d8956b3bb728fcb42e69c0a5eb3de4b5fdfd
sandeeppagi <sandippagi@gmail.com>
2021-05-29 13:32:23 +0530

    BST

---
+++ DS/BinarySearchTree/binary_search_tree_impl.py
@@ -0,0 +1,17 @@
+from DS.BinarySearchTree.node import Node
+
+
+class BinarySearchTree:
+    def __init__(self, val):
+        self.root = Node(val)
+
+    def insert(self, val):
+        if self.root:
+            return self.root.insert(val)
+        else:
+            self.root = Node(val)
+            return True
+
+
+BST = BinarySearchTree(6)
+print(f"Root for BST is {BST.root.val}")
