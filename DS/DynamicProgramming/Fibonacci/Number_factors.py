fa2348250a74a127035185fe792a484865aeb766
Sandy <sandippagi@gmail.com>
2022-02-05 13:45:16 +0530

    Count number of possiable ways

--- DS/DynamicProgramming/Fibonacci/Number_factors.py
+++ DS/DynamicProgramming/Fibonacci/Number_factors.py
@@ -62,3 +62,21 @@ print(count_ways_helper(4))
 print(count_ways_helper(5))
 print(count_ways_helper(6))
 print('-' * 25)
+
+
+def count_ways_bottom_up(n):
+    if n <= 2:
+        return 1
+    if n == 3:
+        return 2
+    dp = [0 for i in range(n + 1)]
+    dp[0], dp[1], dp[2], dp[3] = 1, 1, 1, 2
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 3] + dp[i - 4]
+    return dp[n]
+
+
+print(count_ways_bottom_up(4))
+print(count_ways_bottom_up(5))
+print(count_ways_bottom_up(6))
+print('-' * 25)
